245e0da8c06b5e39328860f925d8917e218b99cc
Kiryuhak <arm2402@yandex.ru>
2024-05-29 21:18:42 +0500

    Fix code

--- Автотестирование API/ApiTesting/config.py
+++ Автотестирование API/ApiTesting/config.py
@@ -1,5 +1,5 @@
-url='https://api.thecatapi.com/v1'
-url_auth='https://api.theauthapi.com'
-key='DEMO-API-KEY'
+url = 'https://api.thecatapi.com/v1'
+url_auth = 'https://api.theauthapi.com'
+key = 'DEMO-API-KEY'
 # key_auth could expire, you need to generate your own key on https://theauthapi.com
-key_auth='live_T941odOy4hS460HfhmmTsDmaZPsmN9eJYrXXjVXnhhco1dChfwsI'
+key_auth = 'live_T941odOy4hS460HfhmmTsDmaZPsmN9eJYrXXjVXnhhco1dChfwsI'
--- Автотестирование API/ApiTesting/data/validate.py
+++ Автотестирование API/ApiTesting/data/validate.py
@@ -2,6 +2,7 @@ import json
 import jsonschema
 from jsonschema import validate
 
+
 def get_schema(name: str) -> dict:
     """This function loads the given schema available"""
     with open(f"data/schemas/{name}.json", 'r') as file:
@@ -10,19 +11,19 @@ def get_schema(name: str) -> dict:
 
 
 def validate_json(name: str, json_data: dict) -> bool:
-	"""REF: https://json-schema.org/ """
-	# Describe what kind of json you expect.
-	execute_api_schema = get_schema(name)	
+    """REF: https://json-schema.org/ """
+    # Describe what kind of json you expect.
+    execute_api_schema = get_schema(name)
 
-	message = f"{name} JSON schema is Valid"
-	state = True
+    message = f"{name} JSON schema is Valid"
+    state = True
 
-	try:
-		validate(instance=json_data, schema=execute_api_schema)
-	except jsonschema.exceptions.ValidationError as err:
-		print(err)
-		message = f"{name} JSON schema is InValid"
-		state = False
+    try:
+        validate(instance=json_data, schema=execute_api_schema)
+    except jsonschema.exceptions.ValidationError as err:
+        print(err)
+        message = f"{name} JSON schema is InValid"
+        state = False
 
-	print (message)
-	return state
+    print(message)
+    return state
